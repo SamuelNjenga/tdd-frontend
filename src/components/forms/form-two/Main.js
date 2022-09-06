dfbd5805d10f3d4fa29d45498c7634a441fcbd5c
Samuel Njenga <snjenga704@gmail.com>
2022-09-06 09:52:32 +0300

    feat: modify Main.test.js by including tests for select drop-downs and onChange event

--- src/components/forms/form-two/Main.js
+++ src/components/forms/form-two/Main.js
@@ -3,6 +3,11 @@ import React, { useState } from "react";
 const Main = () => {
   const [age, setAge] = useState("");
   const [name, setName] = useState("");
+  const [currentLanguage, setCurrentLanguage] = useState("english");
+
+  const changeLanguage = (newLanguage) => {
+    setCurrentLanguage(newLanguage);
+  };
 
   return (
     <div>
@@ -16,6 +21,14 @@ const Main = () => {
         onChange={(e) => setName(e.target.value)}
         placeholder="name"
       />
+      <select
+        onChange={(event) => changeLanguage(event.target.value)}
+        value={currentLanguage}
+      >
+        <option value="spanish">Spanish</option>
+        <option value="chinese" selected>Chinese</option>
+      </select>
+      <p>{currentLanguage}</p>
     </div>
   );
 };
